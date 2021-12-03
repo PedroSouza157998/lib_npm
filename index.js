eb06350cb298120004bbfbb70f5ac39a5080791f
PedroSouza157998 <pedrosouza157998@gmail.com>
2021-12-03 08:49:14 -0300

    add function sum

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 exports.print = function () {
     console.log('Ai que vida dificil')
 }
+exports.sum = function (a) {
+    const sum = a.map((e) => e) 
+    console.log('A soma da porra toda é ', sum)
+}
