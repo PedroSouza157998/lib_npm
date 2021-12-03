a52d0024f6d452a33f0b8b22224555cbbfe30321
PedroSouza157998 <pedrosouza157998@gmail.com>
2021-12-03 09:11:20 -0300

    add generateArrayOfPlans

---
+++ consts.js
@@ -0,0 +1,347 @@
+export const allPlanos = [
+	{
+		label: 'Categorias',
+		resource: 'categorias',
+	},
+	{
+		label: 'Clientes',
+		resource: 'clientes',
+	},
+	{
+		label: 'Dashboard',
+		resource: 'dashboard',
+	},
+	{
+		label: 'Emissão de Pedido',
+		resource: 'pedido',
+	},
+	{
+		label: 'Emissão de Venda',
+		resource: 'venda',
+	},
+	{
+		label: 'Empresa',
+		resource: 'empresa',
+	},
+	{
+		label: 'Produtos',
+		resource: 'produtos',
+	},
+	{
+		label: 'Imagem dos Produtos',
+		resource: 'imagemProdutos',
+	},
+	{
+		label: 'Unidades',
+		resource: 'unidades',
+	},
+	{
+		label: 'Versão Mobile',
+		resource: 'versaoMobile',
+	},
+	{
+		label: 'Versão Web',
+		resource: 'versaoWeb',
+	},
+	{
+		label: 'Emissão de NFC-e mensais',
+		resource: 'nfce',
+	},
+	{
+		label: 'Nº total de Usuários',
+		resource: 'qtdUsuarios',
+	},
+	{
+		label: '(MB) Espaço em Disco',
+		resource: 'armazenamento',
+	},
+	// {
+	// 	label: 'Emissão de NFe',
+	// 	resource: 'nfe',
+	// },
+	// {
+	// 	label: 'Formas de Pagamento',
+	// 	resource: 'formasPagamento',
+	// },
+	// {
+	// 	label: 'Gerenciamento Financeiro',
+	// 	resource: 'financeiro',
+	// },
+	{
+		label: 'Controle de Estoque',
+		resource: 'estoque',
+	},
+	{
+		label: 'Suporte Humanizado',
+		resource: 'suporte',
+	},
+	{
+		label: 'Relatório Básico',
+		resource: 'relatorioBasico',
+	},
+	{
+		label: 'Relatório Avançado',
+		resource: 'relatorioAvancado',
+	},
+	// {
+	// 	label: 'TEF',
+	// 	resource: 'tef',
+	// },
+	{
+		label: 'Treinamento VIP',
+		resource: 'vip',
+	},
+	// {
+	//      label: 'Anúncios',
+	//     resource:
+	// },
+	// {
+	// 	label: 'Histório de Estque',
+	// 	resource: 'historicoEstoque'
+	// },
+	// {
+	// 	label: 'Boleto',
+	// 	resource: 'boleto'
+	// }
+];
+
+export const configAllPlanos = [
+	'pedido',
+	'venda',
+	'relatorioBasico',
+	'clientes',
+	'produtos',
+	'categorias',
+	'nfce',
+	'empresa',
+	'unidades',
+	'formasPagamento',
+	'estoque',
+	'relatorioAvancado',
+	'nfce',
+	'nfe',
+	'tef',
+	'financeiro',
+	'suporte',
+	'qtdUsuarios',
+	'vip',
+	'versaoWeb',
+	'versaoMobile',
+	'armazenamento',
+	'historicoEstoque',
+	'imagemProdutos',
+	'dashboard',
+	'boleto',
+];
+
+export const configsPlanoGratis = [
+	'gratis', //valor relativo ao plano, o qual vai ser diferente dos outros
+	'pedido',
+	'venda',
+	// 'relatorioBasico',
+	'clientes',
+	'produtos',
+	'categorias',
+	'nfce',
+	'empresa',
+	'unidades',
+	// 'formasPagamento',
+	// 'estoque',
+	// 'relatorioAvancado',
+	'nfce',
+	// 'nfe',
+	// 'tef',
+	// 'financeiro',
+	// 'suporte',
+	'qtdUsuarios',
+	// 'vip',
+	'versaoWeb',
+	'versaoMobile',
+	'armazenamento',
+	// 'historicoEstoque',
+	'imagemProdutos',
+	'dashboard',
+	// 'boleto'
+];
+
+export const configsPlanoBronze = [
+	'bronze', //valor relativo ao plano, o qual vai ser diferente dos outros
+	'pedido',
+	'venda',
+	'relatorioBasico',
+	'clientes',
+	'produtos',
+	'categorias',
+	'nfce',
+	'empresa',
+	'unidades',
+	'formasPagamento',
+	'estoque',
+	// 'relatorioAvancado',
+	'nfce',
+	// 'nfe',
+	// 'tef',
+	// 'financeiro',
+	'suporte',
+	'qtdUsuarios',
+	// 'vip',
+	'versaoWeb',
+	'versaoMobile',
+	'armazenamento',
+	// 'historicoEstoque',
+	'imagemProdutos',
+	'dashboard',
+	// 'boleto'
+];
+
+export const configsPlanoSilver = [
+	'silver', //valor relativo ao plano, o qual vai ser diferente dos outros
+	'pedido',
+	'venda',
+	'relatorioBasico',
+	'clientes',
+	'produtos',
+	'categorias',
+	'nfce',
+	'empresa',
+	'unidades',
+	'formasPagamento',
+	'estoque',
+	'relatorioAvancado',
+	'nfce',
+	// 'nfe',
+	// 'tef',
+	// 'financeiro',
+	'suporte',
+	'qtdUsuarios',
+	'vip',
+	'versaoWeb',
+	'versaoMobile',
+	'armazenamento',
+	'historicoEstoque',
+	'imagemProdutos',
+	'dashboard',
+	// 'boleto'
+];
+
+export const configsPlanoGold = [
+	'gold', //valor relativo ao plano, o qual vai ser diferente dos outros
+	'pedido',
+	'venda',
+	'relatorioBasico',
+	'clientes',
+	'produtos',
+	'categorias',
+	'nfce',
+	'empresa',
+	'unidades',
+	'formasPagamento',
+	'estoque',
+	'relatorioAvancado',
+	'nfce',
+	'nfe',
+	'tef',
+	'financeiro',
+	'suporte',
+	'qtdUsuarios',
+	'vip',
+	'versaoWeb',
+	'versaoMobile',
+	'armazenamento',
+	'historicoEstoque',
+	'imagemProdutos',
+	'dashboard',
+	'boleto',
+];
+
+export const plansAnyNonBooleanValue = {
+	qtdUsuarios: {
+		gratis: 1,
+		gratis_marketplace: 1,
+		bronze: 3,
+		bronze_marketplace: 3,
+		silver: 8,
+		gold: 15,
+	},
+	armazenamento: {
+		gratis: '10MB',
+		gratis_marketplace: '10MB',
+		bronze: '30MB',
+		bronze_marketplace: '30MB',
+		silver: '150MB',
+		gold: '300MB',
+	},
+	nfce: {
+		gratis: '10 por mês',
+		gratis_marketplace: '10 por mês',
+		bronze: '100 por mês',
+		bronze_marketplace: '100 por mês',
+		silver: '1000 por mês',
+		gold: '5000 por mês',
+	},
+};
+
+export const plans = [
+	{
+		id: 2,
+		valor: 'R$ 19,90',
+		nome: 'Plano Standard',
+		recursos: [
+			{ nome: 'Sem Anúncios' },
+			{ nome: 'NFC-e Ilimitada' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque ' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Controle de estoque' },
+		],
+		ativo: true,
+	},
+	{
+		id: 3,
+		valor: 'R$ 39,90',
+		nome: 'Plano Confort',
+		ativo: false,
+		recursos: [
+			{ nome: 'Sem Anúncios' },
+			{ nome: 'NFC-e Ilimitada' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Histórico de movimentações' },
+			{ nome: 'Relatórios Básicos', emBreve: true },
+			{ nome: 'TEF', emBreve: true },
+		],
+	},
+	{
+		id: 4,
+		valor: 'R$ 59,90',
+		nome: 'Plano Master',
+		recomendado: true,
+		ativo: false,
+		recursos: [
+			{ nome: 'Sem Anúncios' },
+			{ nome: 'NFC-e Ilimitada' },
+			{ nome: 'Controle de estoque' },
+			{ nome: 'Histórico de movimentações', emBreve: true },
+			{ nome: 'Relatórios Básicos', emBreve: true },
+			{ nome: 'TEF', emBreve: true },
+			{ nome: 'Emissão de NFe', emBreve: true },
+			{ nome: 'Relatórios Avançados', emBreve: true },
+			{ nome: "Armazenamento de XML's", emBreve: true },
+		],
+	},
+];
--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import {
+	allPlanos,
+	configsPlanoBronze,
+	configsPlanoGold,
+	configsPlanoGratis,
+	configsPlanoSilver,
+	plansAnyNonBooleanValue,
+} from './consts';
+
 exports.print = function () {
     console.log('Ai que vida dificil')
 }
@@ -5,4 +14,43 @@ exports.sum = function (a) {
     let sum = 0 
     a.map((e) => sum = sum+e)
     console.log('A soma da porra toda é ', sum)
+}
+
+exports.generateArrayOfPlans =  function (plan) {
+	//pega o primeiro elemento do array de planos (gratis, bronze, silver, gold)
+	let planToFilter = 'gratis';
+	
+	switch (plan) {
+		case 'gratis':
+			planToFilter = configsPlanoGratis;
+			break;
+		case 'bronze':
+			planToFilter = configsPlanoBronze;
+			break;
+		case 'silver':
+			planToFilter = configsPlanoSilver;
+			break;
+		case 'gold':
+			planToFilter = configsPlanoGold;
+			break;
+	}
+
+	const firstPlanArrayElement = planToFilter[0];
+
+	const copyAllPlanos = JSON.parse(JSON.stringify(allPlanos));
+
+	return copyAllPlanos.map(plan => {
+		//define se o recurso está ativo no plano
+		plan.enabled = planToFilter.some(f => f == plan.resource);
+
+		//alguns valores dos recursos não são booleanos. Por isso é feita essa verificação para
+		//concatenar no "label" o valor do recurso, como também salvar o próprio valor
+		if (plansAnyNonBooleanValue[plan.resource]) {
+			const objValue = plansAnyNonBooleanValue[plan.resource][firstPlanArrayElement]; //prettier-ignore
+			plan.label = /* (${objValue}) */ `${plan.label}`;
+			plan.nonBooleanValue = Number(String(objValue).replace(/\D/g, ''));
+		}
+
+		return plan;
+	});
 }
